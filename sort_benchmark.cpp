dc6ed0b38580c8f4d28c771deb612f3716184b66
Matthew Brewer <mbrewer@meteor.com>
2017-02-08 13:46:12 -0500

    Sort benchnmarks!

---
+++ sort_benchmark.cpp
@@ -0,0 +1,28 @@
+#include "sort.h"
+#include "array.h"
+
+#define TEST_SIZE 10000000
+#define PASSES 100
+
+class IntComparitor {
+  public:
+    static int compare(int v1, int v2) {
+      return v1 - v2;
+    }
+};
+
+int main(int argc, char **argv) {
+  printf("Begin sort benchmark\n");
+  Array<int> a(TEST_SIZE);
+  Array<int> b(TEST_SIZE);
+  for (int j = 0; j<PASSES; j++) {
+    for (int i = 0; i<TEST_SIZE; i++) {
+      a[i] = rand();
+    }
+    //quick_sort<Array<int>, IntComparitor>(&a);
+    merge_sort<Array<int>,Array<int>, IntComparitor>(&a, &b);
+  }
+  printf("Done\n");
+  return 0;
+}
+
