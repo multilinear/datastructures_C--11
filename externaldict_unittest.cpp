bad889413c6916c8a0d8fe10d9949c193abeb924
Matthew Brewer <mbrewer@meteor.com>
2017-03-25 19:24:38 +0000

    Missing files

---
+++ externaldict_unittest.cpp
@@ -0,0 +1,244 @@
+/* Copywrite Matthew Brewer 2017-03-22
+ *
+ * This is a unittest for dictionaries using external allocation
+ * We decide WHAT we're testing using the macro system
+ * Our Makefile takes advantage of this.
+ *
+ * This seems odd, but it avoids code duplication and ensures all our
+ * datastructures have the same API (Except where it really isn't a good idea)
+ */
+
+#include <stdio.h>
+#include "panic.h"
+#include "dlist.h"
+
+#define TEST_SIZE 200
+
+#ifdef TEST_AVL
+// This turns on rather expensive internal consistancy checking
+#define DEBUG_AVL
+#include "avl.h"
+#define SORTED_ITERATOR
+class Node: public AVLNode_base<Node, int> {
+#endif
+#ifdef TEST_AVLHASHTABLE
+// This turns on rather expensive internal consistancy checking
+#define DEBUG_AVLHASHTABLE
+#include "avlhashtable.h"
+class Node: public AVLHashTableNode_base<Node, int> {
+#endif
+#ifdef TEST_OCHASHTABLE
+// This turns on rather expensive internal consistancy checking
+#define DEBUG_OCHASHTABLE
+#include "ochashtable.h"
+class Node: public OCHashTableNode_base<Node> {
+#endif
+#ifdef TEST_REDBLACK
+// This turns on rather expensive internal consistancy checking
+#define DEBUG_REDBLACK
+#include "redblack.h"
+#define SORTED_ITERATOR
+class Node: public RedBlackNode_base<Node, int> {
+#endif
+#ifdef TEST_RREDBLACK
+// This turns on rather expensive internal consistancy checking
+#define DEBUG_RREDBLACK
+#include "rredblack.h"
+#define SORTED_ITERATOR
+class Node: public RRedBlackNode_base<Node, int> {
+#endif
+  public:
+    int value;
+  public:
+    const int val(void) const {
+      return value;
+    }
+    static size_t hash(int v) {
+      return v;
+    }
+    static int compare(const int v1, const int v2) {
+      return v1-v2;
+    }
+    Node() {}
+    void set(int v) {
+      value = v;
+    }
+    Node(int v) {
+      value = v;
+    }
+    void print(void) const {
+      printf("%d", value);
+    }
+};
+
+class TNode: public DListNode_base<TNode> {
+  public:
+    int value;
+  public:
+    TNode() { }
+    TNode(int i) {
+      value = i;
+    }
+    const int val(void) {
+      return value;
+    }
+    static const int compare(const int val1, const int val2) {
+      return val1-val2;
+    }
+    void print(void) {
+      printf("%d", value);
+    }
+};
+
+template<typename DT>
+void check(DT *dict, DList<TNode,int> *tdict) {
+  auto i = tdict->begin();
+  for (; i != tdict->end(); i++) {
+    if(!dict->get(i->val())) {
+      printf("value = %d\n", i->val());
+      PANIC("Element not in dict anymore!");
+    }
+  }
+  auto j = dict->begin();
+  for (; j != dict->end(); j++) {
+    if (!tdict->get(j->val())) {
+      printf("value = %d\n", j->val());
+      PANIC("Element in dict, but should be!");
+    }
+  }
+}
+
+int main(int argc, char* argv[]) {
+  DList<TNode, int> tdict;
+  #ifdef TEST_OCHASHTABLE
+  printf("Begin OCHashTable.h unittest\n");
+  OCHashTable<Node, int> dict;
+  #endif
+  #ifdef TEST_AVLHASHTABLE
+  printf("Begin AVLHashTable.h unittest\n");
+  AVLHashTable<Node, int> dict;
+  #endif
+  #ifdef TEST_AVL
+  printf("Begin AVL.h unittest\n");
+  AVL<Node, int> dict;
+  #endif
+  #ifdef TEST_REDBLACK
+  printf("Begin RedBlack.h unittest\n");
+  RedBlack<Node, int> dict;
+  #endif
+  #ifdef TEST_RREDBLACK
+  printf("Begin RRedBlack.h unittest\n");
+  RRedBlack<Node, int> dict;
+  #endif
+
+  int i;
+  // insert in order, then remove
+  int k;
+  for (k=1; k<TEST_SIZE; k++) {
+    for (i=0; i<k; i++) {
+      bool new_v = false;
+      int r;
+      // find a value we haven't used yet
+      while (!new_v) {
+        // Note, we did not initialize rand, this is purposeful
+        r = rand();
+        new_v = true;
+        new_v = !tdict.get(r);
+      }
+      // put it in thet dict
+      Node *n = new Node(r);
+      dict.insert(n);
+      // and in the list
+      tdict.insert(new TNode(r));
+      // check that everything is in the list that should be
+      check<decltype(dict)>(&dict, &tdict);
+    }
+    while (!tdict.isempty()) {
+      // We're invalidating our iterator every round, by modifying the dict
+      auto i = tdict.begin();
+      auto n = dict.get(i->val());
+      if (!n) {
+        PANIC("Node not found\n");
+      }
+      #ifdef TEST_RREDBLACK
+      dict.remove(i->val());
+      #else
+      n = dict.get(i->val());
+      dict.remove(n);
+      delete n;
+      #endif
+      auto tn = &(*i);
+      tdict.remove(tn);
+      delete tn;
+      // check that everything is in the list that should be
+      check<decltype(dict)>(&dict, &tdict);
+    }
+    if (!dict.isempty()) {
+      PANIC("Tree should be empty here, but isn't");
+    }
+  }
+  
+  // Check non-existant element
+  Node *n = dict.get(1); 
+  if (n != nullptr) {
+    PANIC("Underflow returned a value!");
+  }
+
+  // Check duplicate element behavior
+  n = new Node(1);
+  bool inserted = dict.insert(n);
+  if (!inserted) {
+    PANIC("Insertion failed");
+  }
+  #ifndef TEST_RREDBLACK
+  n = new Node(1);
+  inserted = dict.insert(n);
+  if (inserted) {
+    PANIC("Double insertion succeeded");
+  }
+  #endif
+  // We didn't bother making rredblack handle double insertion elegantly
+  n = dict.get(1);
+  if (n == nullptr) {
+    PANIC("Attempted insertion removed existing data");
+  }
+  n = dict.get(1);
+  // RRedblack has no reason to do node based removal, value-based is just as fast
+  #ifdef TEST_RREDBLACK
+  dict.remove(n->val());
+  #else
+  dict.remove(n);
+  delete n;
+  #endif
+  n = dict.get(1);
+  if (n != nullptr) {
+    PANIC("underflow returned a value after duplicate insert");
+  }
+  // Test SORTED iterator
+  #ifdef SORTED_ITERATOR
+  for (size_t i=0; i<100; i++) {
+    dict.insert(new Node(i));
+  }
+  i = 0;
+  for (auto i2 = dict.begin(); i2 != dict.end(); ++i2) {
+    if (i2->val() != i) {
+      dict.print();
+      printf("%d should be %d\n", i2->val(), i);
+      PANIC("dict iterator broken");
+    }
+    ++i;
+  }
+  // drain the dictionary
+  auto a = dict.begin();
+  while (a != dict.end()) {
+    #ifdef TEST_RREDBLACK
+    dict.remove(a->val());
+    #else
+    dict.remove(&(*a));
+    delete (&(*a));
+    #endif
+    a = dict.begin();
+  }
+  #endif
+  printf("PASS\n");
+}
---
+++ internaldict_unittest.cpp
@@ -0,0 +1,195 @@
+#include <stdio.h>
+#include "panic.h"
+#include "dlist.h"
+
+#define TEST_SIZE 200
+
+#ifdef TEST_BTREE
+// This turns on rather expensive internal consistancy checking
+#define BTREE_DEBUG
+// This turns on the iterator (adding a parent pointer)
+#define BTREE_ITERATOR
+#include "btree.h"
+#define ARITY 5
+#endif
+
+#ifdef TEST_TS_BTREE
+#define BTREE_DEBUG
+#include "ts_btree.h"
+#define ARITY 5
+#endif
+
+#ifdef TEST_BTREEHASHTABLE
+#include "btreehashtable.h"
+#endif
+
+class Comp {
+  // For use with T=int, Val_T=int
+  public:
+    static const int val(const int T) {
+      return T;
+    }
+    static const int compare(const int val1, const int val2) {
+      return val1-val2;
+    }
+    static size_t hash(int v) {
+      return v;
+    }
+    static void printT(const int t) {
+      printf("%d", t);
+    }
+    static void printV(const int v) {
+      printf("%d", v);
+    }
+};
+
+class TNode: public DListNode_base<TNode> {
+  public:
+    int value;
+  public:
+    TNode() { }
+    TNode(int i) {
+      value = i;
+    }
+    const int val(void) {
+      return value;
+    }
+    static const int compare(const int val1, const int val2) {
+      return val1-val2;
+    }
+    void print(void) {
+      printf("%d", value);
+    }
+};
+
+template<typename DT>
+void check(DT *dict, DList<TNode,int> *tdict) {
+  auto i = tdict->begin();
+  for (; i != tdict->end(); i++) {
+    #ifdef TEST_TS_BTREE
+    int res;
+    if(!dict->get(i->val(), &res)) {
+    #else
+    if(!dict->get(i->val())) {
+    #endif
+      printf("%d\n", i->val());
+      PANIC("Element not in dict anymore!");
+    }
+  }
+}
+
+int main(int argc, char* argv[]) {
+  DList<TNode, int> tdict;
+
+  #ifdef TEST_BTREE
+  printf("Begin BTree.h unittest\n");
+  BTree<int, int, Comp, ARITY> dict;
+  #endif
+  #ifdef TEST_TS_BTREE
+  printf("Begin TS_BTree.h unittest\n");
+  TSBTree<int, int, Comp, ARITY> dict;
+  #endif
+  #ifdef TEST_BTREEHASHTABLE
+  printf("Begin BTreeHashTable.h unittest\n");
+  BTreeHashTable<int, int, Comp> dict;
+  #endif
+
+  int i;
+  // insert in order, then remove
+  int k;
+  for (k=1; k<TEST_SIZE; k++) {
+    //printf("**** Adding elements\n");
+    for (i=0; i<k; i++) {
+      bool new_v = false;
+      int r;
+      // find a value we haven't used yet
+      while (!new_v) {
+        // Note, we did not initialize rand, this is purposeful
+        r = rand();
+        new_v = true;
+        new_v = !tdict.get(r);
+      }
+      dict.insert(r);
+      tdict.insert(new TNode(r));
+      check<decltype(dict)>(&dict, &tdict);
+    }
+    //printf("**** Draining\n");
+    while (!tdict.isempty()) {
+      // We're invalidating our iterator every round, by modifying the dict
+      // but we removed the first element, so we just start over again
+      auto i = tdict.begin();
+      int d;
+      bool f;
+      //printf("removing %d\n", i->val());
+      #ifdef TEST_TS_BTREE
+      int res;
+      f = !!dict.get(i->val(), &res);
+      #else
+      f = !!dict.get(i->val());
+      #endif
+      if (!f) {
+        PANIC("dict lacks element it should have");
+      }
+      dict.remove(i->val(), &d);
+      #ifdef TEST_TS_BTREE
+      f = !!dict.get(i->val(), &res);
+      #else
+      f = !!dict.get(i->val());
+      #endif
+      if (f) {
+        printf("Element %d\n", i->val());
+        PANIC("dict has element it should not have");
+      }
+			auto tn = &(*i);
+      tdict.remove(tn);
+			delete tn;
+      check<decltype(dict)>(&dict, &tdict);
+    }
+    //printf("Checking  that things are empty\n");
+    if (!dict.isempty()) {
+      PANIC("Dict should be empty here, but isn't");
+    }
+    //printf("************ size %d complete\n", k);
+  }
+  // Check duplicate behavior
+  if (!dict.insert(1))  {
+    PANIC("Dict insert of single value failed");
+  }
+  if (dict.insert(1)) {
+    PANIC("Dict insert of duplicate succeeded");
+  }
+  if (dict.isempty()) {
+    PANIC("Dict thinks it's empty");
+  }
+  int v;
+  if (!dict.remove(1, &v)) {
+    PANIC("Dict remove of first after duplicate insert failed");
+  }
+  if (dict.remove(1, &v)) {
+    PANIC("Dict remove of first after duplicate insert failed");
+  }
+  if (!dict.isempty()) {
+    dict.print();
+    PANIC("Dict thinks it's not empty");
+  }
+  #ifndef TEST_TS_BTREE
+  // Test iterator
+  for (size_t i=0; i<100; i++) {
+    dict.insert(i);
+  }
+  i = 0;
+  for (auto i2 = dict.begin(); i2 != dict.end(); ++i2) {
+    if (*i2 != i) {
+      printf("%d should be %d\n", *i2, i);
+      PANIC("dict iterator broken");
+    }
+    ++i;
+  }
+  int val;
+  auto a = dict.begin();
+  while (a != dict.end() && dict.remove(*a, &val)) {
+    a = dict.begin();
+  }
+  #endif
+  printf("PASS\n");
+}
