bd14b898f151ba1dfab29938b995d7a134a5d64a
Matthew Brewer <mbrewer@smalladventures.net>
2018-12-17 10:48:26 -0500

    Add utilities for graphing arity benchmarks

---
+++ arity_benchmark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+target=$1 
+
+total=134217728
+size=2;
+iterations=1
+rm log
+while [[ ${iterations} != 0 ]]; do 
+  size=$((size+size))
+  iterations=$((total/size))
+  export TEST_ITERATIONS=${iterations}
+  export TEST_SIZE=${size}
+  for ((arity=2;arity<9;arity++)); do
+    make clean
+    TEST_ITERATIONS=${iterations} TEST_SIZE=${size} ARITY=${arity} make -e ${1} &>> log
+  done
+done
